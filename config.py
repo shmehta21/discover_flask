51a8feae394423dc3ef587e8c1b707c0f445bc47
Sagar <shmehta21@gmail.com>
2019-04-27 16:47:11 +0530

    adding migrations to database

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class BaseConfig(object):
+	DEBUG = False
+	SECRET_KEY = 'O\x88\x11G\x9b\x11\xc2\x88\x9fg\x92Y\xeb)\xae\xd8\xb4W+7\x879K\x81'
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	print(SQLALCHEMY_DATABASE_URI)
+
+class DevelopmentConfig(BaseConfig):
+	DEBUG = True
+
+class ProductionConfig(BaseConfig):
+	DEBUG = True
+
+
+
+
---
+++ db_create.py
@@ -0,0 +1,17 @@
+from app import db
+from models import BlogPost
+
+
+#create the database and the db tables
+db.create_all()
+
+#insert
+db.session.add(BlogPost("Good","I\'m good."))
+db.session.add(BlogPost("Better","I\'m better."))
+db.session.add(BlogPost("Best","I\'m best."))
+db.session.add(BlogPost("postgres","We setup a local postgresql instance"))
+db.session.add(BlogPost("postgres local","We setup a local postgresql instance"))
+db.session.add(BlogPost("Test","Test commit on GIT"))
+
+#commit the changes
+db.session.commit()

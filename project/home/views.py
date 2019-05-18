4d51addb69de4a14c701a0c62b29e07e75881bcf
Sagar <shmehta21@gmail.com>
2019-05-18 12:45:54 +0530

    Added funcitonality to add a new Post

--- project/home/views.py
+++ project/home/views.py
@@ -4,8 +4,9 @@
 #########################
 from project import app, db
 from project.models import BlogPost
-from flask import render_template, Blueprint
-from flask_login import login_required
+from project.home.forms import MessageForm
+from flask import render_template, Blueprint, request, flash, redirect, url_for
+from flask_login import login_required, current_user
 #from functools import wraps
 
 ########################
@@ -35,11 +36,25 @@ def login_required(f):
 ###### routes #########
 #######################
 
-@home_blueprint.route('/')
+@home_blueprint.route('/', methods=['GET','POST'])
 @login_required
 def home():
-	posts = db.session.query(BlogPost).all()
-	return render_template('index.html', posts = posts)
+	error = None
+	form = MessageForm(request.form)
+	if form.validate_on_submit():
+		new_message = BlogPost(
+				form.title.data,
+				form.description.data,
+				current_user.id
+			)
+		db.session.add(new_message)
+		db.session.commit()
+		flash('New entry was successfully posted. Thanks!')
+		return redirect(url_for('home.home'))
+
+	else:
+		posts = db.session.query(BlogPost).all()
+		return render_template('index.html', posts=posts, form=form, error=error)
 
 @home_blueprint.route('/welcome')
 def welcome():
--- project/users/forms.py
+++ project/users/forms.py
@@ -1,12 +1,12 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
 	username = TextField( 'Username', validators=[DataRequired()] )
 	password = PasswordField('Password', validators=[DataRequired()] )
 
-class RegisterForm(Form):
+class RegisterForm(FlaskForm):
 	username=TextField(
 			 'username',
 			 validators=[DataRequired(), Length(min=3, max=25)]
